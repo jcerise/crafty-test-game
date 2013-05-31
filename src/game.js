c4dc8e7ef05423350fcb81bea60a01157b4d0c30
jcerise <jcerise06@gmail.com>
2013-05-31 15:10:58 -0600

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,8 @@
+Game = {
+    //Initialize and start our game
+    start: function() {
+        //Start Crafty and set a background color, so we know it is working
+        Crafty.init(480, 320);
+        Crafty.background('green');
+    }
+}
